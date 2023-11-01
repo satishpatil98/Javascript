56be2cfd3ed179800c1991698d86f13ed7a82e5c
satishpatil98 <149506430+satishpatil98@users.noreply.github.com>
2023-11-01 17:26:32 +0000

    Lecture Data types

--- Variables.js
+++ Variables.js
@@ -1,1 +1,23 @@
 console.log("Hello world")
+
+console.log("Data Types")
+
+console.log("String")
+// Premitive type
+// String
+// Number
+// Boolean
+// Undefined
+// null
+
+// Non Premitive types 
+// Objects 
+
+
+
+
+// What is mutable and immutable 
+
+// Memory Management stake and Heap 
+
+
