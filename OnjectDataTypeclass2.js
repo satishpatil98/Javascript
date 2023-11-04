584162ba49bafa08b378bf1fde2b17cb599ea333
satishpatil98 <149506430+satishpatil98@users.noreply.github.com>
2023-11-04 17:35:22 +0000

    JSObjectsClass2

---
+++ OnjectDataTypeclass2.js
@@ -0,0 +1,36 @@
+// Deleting properties of object
+// delete objectName.propertyName;
+
+
+//Checking if a property exists 
+// property in ObjectName
+
+// What is type of Object
+
+
+// object.Keys(objectName)
+// Object.Values(ObjectName)
+
+
+// Object Destructuring 
+
+// Object.entries(obj)
+
+// Creating Objects from Another array 
+
+//Object.prototype.hasOwnProperty(). or hasOwn syntax (objectName, propertyName)
+
+// JSON Object and its usages 
+
+// What is prototype 
+// Definitions : Every object has its own property called a prototype.  objects can inherit features from one another via prototypes. 
+// Prototype Chain : Because a prototype itself is also another object, the prototype has its own prototype. This creates a something called prototype chain. The prototype chain ends when a prototype has null for its own prototype.
+
+
+// type of Object
+
+
+
+
+
+
