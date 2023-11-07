2b42eebb18d5a6d826e1022036db37b9de164a60
satishpatil98 <149506430+satishpatil98@users.noreply.github.com>
2023-11-07 03:05:56 +0000

    hiesting

--- Hoisting.js
+++ Hoisting.js
@@ -1,4 +1,3 @@
 // What is Hoisting,  declaration of variables and function is put/move on top by compiler or interpreter befor runing the code 
 
 // What can be Hoisting , only Var and function declaration can be hosted 
-
