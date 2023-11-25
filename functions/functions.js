2b4474c3c7e4f6d66243893fd4322f90d15488dd
satishpatil98 <149506430+satishpatil98@users.noreply.github.com>
2023-11-25 12:00:19 +0000

    Function cllass 1

--- functions/functions.js
+++ functions/functions.js
@@ -6,12 +6,59 @@
 // this is called functin declaration 
 function nameofFunction1 (){ // function with zeror parameters  and retun nothing 
     // function body
+    console.log("Hello Function")
 }
+
+// Function calling 
+//nameofFunction1();
+
+
 
 function nameofFunction2 (param1 ,param2 ,param3 ){ // function with  parameters  and retun addition of parameters  
     // function body
-    return param1 + param2 + param3
+    try {
+        let sum = param1 + param2 + param3 ;   
+       // console.log("sum =", sum)
+
+        return sum
+    } catch (error) {
+        
+    }
+
+    
 }
+
+
+function fullname (fname ="Satish", lname="Patil" ){ 
+    let full_name = fname + " " + lname
+    return full_name;
+}
+
+
+let full_name_output = fullname ("Ganesh" , "Shinde")
+// console.log(" function call output ::  ",full_name_output)
+
+let fname2 = fullname()
+//console.log(" function call output 2::  ",fname2)
+
+let fname3 = fullname("Ganesh")
+//console.log(" function call output 3::  ",fname3)
+
+let fullnameFunction = fullname // Reference of function 
+
+//console.log(" call with varibale ",fullnameFunction("Rahul" , " Dravid" ))
+
+
+
+
+
+
+
+
+// function call 2
+
+//console.log("nameofFunction2 :: ",nameofFunction2(1,2,3))
+
 
 
 // What is parameters and what is arguments 
@@ -22,42 +69,52 @@ function sum (number1,number2){ // number1 , number2 are parameters  means varia
 }
 
 
-console.log("sum of 5 and 10 is =", sum(5,10)) 
+//console.log("sum of 5 and 10 is =", sum(5,10)) 
 
 
 // what is difference in function reference and function call
 
-let sumref = sum // this is function reference 
+let sumref = sum // this is function reference  // Function will not get called only recterence is assinged to varibale 
 
 let sumofnumber = sum(4,5) // this is called fuction calling 
 
-console.log("normal function call ",sumofnumber)
-let sumeofnumber2 = sumref(10,20)
-console.log("reference function call ",sumeofnumber2)
+//console.log("normal function call ",sumofnumber)
+let sumeofnumber2 = sumref(10,20) /// actual function call wo=ith refence name 
+//console.log("reference function call ",sumeofnumber2)
 
 
 // Can we declare function inside another function ? 
+//IMP  :: react 
 
-function nestedfunctions(){
-    
+function nestedfunctions(){ // Function defined 
+    // Code 
+    innnerfunction();
     console.log("outer function")
-    function innnerfunction(){
+    
+    function innnerfunction(){ // Function defined 
         console.log("innnerfunction")
     }
-  //  innnerfunction();
+    innnerfunction();
 }
 
-nestedfunctions()
+//nestedfunctions() // Function call
 
 
 // Arrow function 
 
 // syntax  
-funname =()=>{
+funname =(param1,param2)=>{
 console.log("code logic ") 
 }
 
-funname();
+/*
+
+function funname (param1,param2){
+  console.log("code logic ")   
+}
+*/
+
+funname(); // function call 
 
 
 
@@ -65,4 +122,18 @@ funname();
 
 
 
+
+
+const myObj = {
+    name: "John",
+    age: 30,
+    cars: [
+      {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+      {name:"BMW", models:["320", "X3", "X5"]},
+      {name:"Fiat", models:["500", "Panda"]}
+    ]
+  }
+
+
+  //console.log (Object.keys(myObj))
 
