afb66a8ef164c55b4ca17c1891f474ec335f3df6
satishpatil98 <149506430+satishpatil98@users.noreply.github.com>
2023-11-28 17:29:13 +0000

    hoisting

--- Hoisting.js
+++ Hoisting.js
@@ -1,3 +1,14 @@
-// What is Hoisting,  declaration of variables and function is put/move on top by compiler or interpreter befor runing the code 
+// What is Hoisting,  declaration of variables and function is put/move on top by compiler or interpreter before runing the code 
 
 // What can be Hoisting , only Var and function declaration can be hosted 
+
+//In terms of variables and constants, keyword var is hoisted and let and const does not allow hoisting.
+
+// in JavaScript, initializations are not hoisted.
+
+// in JavaScript, initializations are not hoisted.
+//when the variable is used inside the function, the variable is hoisted only to the top of the function
+//Function Hoisting 
+// Can function expression be hosted ?
+// can arrow function hosted ?
+// As only Javascript is support the hoisting ,it advise to declare variables and constants before using it.  
