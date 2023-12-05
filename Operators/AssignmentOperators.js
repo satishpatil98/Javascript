7b82548b0050abd67409021377d8f3397846de88
satishpatil98 <149506430+satishpatil98@users.noreply.github.com>
2023-12-05 16:54:02 +0000

    OPERATORS

---
+++ Operators/AssignmentOperators.js
@@ -0,0 +1,36 @@
+// Assignment operators are user to assign the lave or expresions from right hand side of operator (=) to its left hand side variabl
+
+// examples 
+let x = 10 ; // here = is assignment operator  and 10 is assigned to x variable 
+
+// additions assignment -->
+
+ x += 10 // is equal to x = x+10
+
+ // Substraction assignemet operator
+
+ x-=5 // is qual to x = x-5
+
+ // multiplication assignement
+
+ let y = 2
+
+ y *=2 // equal to y = y * 2 
+ 
+ // divisoin assignment
+
+ y /=2 // equal to y = y/2
+
+ // esctructuring assignemnt 
+
+ let x1 = ["one1","two2","three3","four4"]
+/*
+const one = foo[0];
+const two = foo[1];
+const three = foo[2];
+const four = foo[3];
+*/
+
+const[one,two,three,four] = x1
+
+console.log(one,two,three,four)
